bc2b694b79cbf2e6e197e5197c49506e460178fd
Raviraj Pawar <pawarraviraj9160@gmail.com>
2020-08-11 13:57:08 +0530

    Update Config.py
    hided application configuration

--- Config.py
+++ Config.py
@@ -1,62 +1,62 @@
-class Config(object):
+ouclass Config(object):
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
     # Normal Configurtion
-    SECRET_KEY = "raviraj+Amir+both+developed"
+    SECRET_KEY = "Make_Your_Own_Key"
     UPLOAD_FOLDER = r'Uploaded Documnet//'
 
     # Mail Server configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'xpressxeroxx@gmail.com'
-    MAIL_PASSWORD = 'AmirAdmin'
+    MAIL_USERNAME = 'Your_Gmail@gmail.com'
+    MAIL_PASSWORD = 'Password_of_Your_Gmail'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
     # Database configuration
-    MYSQL_HOST = 'xpressxerox.mysql.pythonanywhere-services.com'
-    MYSQL_USER = 'xpressxerox'
-    MYSQL_PASSWORD = 'rootroot'
-    MYSQL_DB = 'xpressxerox$Amir'
+    MYSQL_HOST = 'Localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'YourDB'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SECRET_KEY = "raviraj+Amir+both+developed"
+    SECRET_KEY = "Make_Your_Own_Key"
     UPLOAD_FOLDER = r'Uploaded Documnet//'
 
     # Mail Server configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'xpressxeroxx@gmail.com'
-    MAIL_PASSWORD = 'AmirAdmin'
+    MAIL_USERNAME = 'Your_Gmail@gmail.com'
+    MAIL_PASSWORD = 'Password_of_Your_Gmail'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
     # Database configuration
-    MYSQL_HOST = 'xpressxerox.mysql.pythonanywhere-services.com'
-    MYSQL_USER = 'xpressxerox'
-    MYSQL_PASSWORD = 'rootroot'
-    MYSQL_DB = 'xpressxerox$Amir'
+    MYSQL_HOST = 'Localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'YourDB'
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-    SECRET_KEY = "raviraj+Amir+both+developed"
+    SECRET_KEY = "Make_Your_Own_Key"
     UPLOAD_FOLDER = r'Uploaded Documnet//'
 
     # Mail Server configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'xpressxeroxx@gmail.com'
-    MAIL_PASSWORD = 'AmirAdmin'
+    MAIL_USERNAME = 'Your_Gmail@gmail.com'
+    MAIL_PASSWORD = 'Password_of_Your_Gmail'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
     # Database configuration
-    MYSQL_HOST = 'xpressxerox.mysql.pythonanywhere-services.com'
-    MYSQL_USER = 'xpressxerox'
-    MYSQL_PASSWORD = 'rootroot'
-    MYSQL_DB = 'xpressxerox$Amir'
+    MYSQL_HOST = 'Localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'YourDB'
